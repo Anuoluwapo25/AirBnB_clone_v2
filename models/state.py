60a2e8f7e8ba2e188d0d71427d13f401550a606f
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-03-31 08:29:04 -0700

    database

--- models/state.py
+++ models/state.py
@@ -12,20 +12,26 @@ storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
 class State(BaseModel, Base):
     """State class handles all application states"""
-        __tablename__ = 'states'
-        if (os.getenv("HNBN_TYPE_STORAGE") == 'db'):
+    if (getenv("HNBN_TYPE_STORAGE") == 'db'):
+         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state', cascade='all delete')
     else:
         name = ''
+        cities = []
+    
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
 
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
-        def cities(self):
+        def cities(self) -> list:
             """
             return list of cities
             """
             my_list = []
-            for key, value in models.storage.all("City"):
+            for value in list(models.storage.all("City").value()):
                 if (value.state_id == self.id):
                     my_list.append(value)
             return (my_list)
