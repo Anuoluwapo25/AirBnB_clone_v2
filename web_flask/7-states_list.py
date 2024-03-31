bef88a43da1d10f9088efe4928405c26cb653607
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-03-31 07:51:53 -0700

    list of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""
+
+from flask import Flask
+from models import storage, State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """
+    remove the current session
+    """
+    storage.close()
+
+
+
+app.route("/states_list", strict_slashes=False)
+def states_lists():
+    """
+    dispaly HtMl PAGE
+    """
+    states = storage.all(State).values()
+    sorted_state = sorted(states, key=lambda state: state.name)
+    return render_template("7-states_list.html", states=sorted_state)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
